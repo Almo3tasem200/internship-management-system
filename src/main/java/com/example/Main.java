820c511f981549492d9e45f7c1c8e1ebe9818261
Elmo3tasem <moatasem.elemam20@gmail.com>
2026-07-14 21:24:02 +0300

    Create a Java program that connects to the PostgreSQL database successfully.

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,17 +1,21 @@
 package com.example;
 
+import java.sql.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    static void main() throws SQLException {
+        Connection conn = DriverManager.getConnection
+                ("jdbc:postgresql://localhost:5432/postgres",
+                        "postgres", "12345");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        String sql = "SELECT trackId, trackName FROM track";
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        while (rs.next()) {
+            System.out.println(rs.getString("trackName"));
         }
+
     }
 }
